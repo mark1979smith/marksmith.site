c97cb86dc431de5dc8753e4e87912c18615c2e55
Mark Smith <mark1979smith@googlemail.com>
2017-09-14 19:55:55 +0100

    Checked if is admin in controller

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -8,9 +8,9 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Utils\User;
+use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -23,10 +23,20 @@ class AdminController extends Controller
     /**
      * @Route("/admin", name="admin-home")
      */
-    public function indexAction(Request $request)
+    public function indexAction(Request $request, LoggerInterface $logger)
     {
-        return $this->render('admin/index.html.twig', [
-        ]);
+        /** @var \AppBundle\Utils\Api\Redis $api */
+        $api = $this->get('app.api.redis');
+
+        $user = new User();
+        $userData = $user->isLoggedIn($request, $api, $logger);
+
+        if ($userData['result'] && $userData['contents']->admin === true) {
+            return $this->render('admin/index.html.twig', [
+            ]);
+        } else {
+            return $this->redirectToRoute('homepage');
+        }
     }
 
 }
